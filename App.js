03b9e4f582dc11ae767b9c29ae7b620f521f4741
yogieputra8 <yogie.putra8@gmail.com>
2018-12-18 21:09:58 +0700

    navigate to others page

--- App.js
+++ App.js
@@ -1,34 +1,55 @@
 import React, { Component } from "react"
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native"
+import GetPage from "./src/GetPage"
+import PutPage from "./src/PutPage"
+import PostPage from "./src/PostPage"
+import DeletePage from "./src/DeletePage"
 
 class App extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      pageWillBeRendered: "main"
+    }
   }
 
-  openGetPage = () => {
-    
+  openOtherPage = page => {
+    if (page && page !== "") {
+      this.setState({ pageWillBeRendered: page })
+    }
   }
 
   render() {
+    const { pageWillBeRendered } = this.state
+
+    if (pageWillBeRendered === "get") {
+      return <GetPage />
+    } else if (pageWillBeRendered === "post") {
+      return <PostPage />
+    } else if (pageWillBeRendered === "put") {
+      return <PutPage />
+    } else if (pageWillBeRendered === "delete") {
+      return <DeletePage />
+    }
+
     return (
       <View>
         <Text>Networking App</Text>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => this.openOtherPage("get")}>
           <Text style={Styles.txtButton}>Open Get Page</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => this.openOtherPage("post")}>
           <Text style={Styles.txtButton}>Open Post Page</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => this.openOtherPage("put")}>
           <Text style={Styles.txtButton}>Open Put Page</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => this.openOtherPage("delete")}>
           <Text style={Styles.txtButton}>Open Delete Page</Text>
         </TouchableOpacity>
       </View>
