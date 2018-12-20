c20d837edbe762a80ef9aff5b1667c3c2248879a
yogieputra8 <yogie.putra8@gmail.com>
2018-12-20 20:03:03 +0700

    completed post request

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,16 +1,81 @@
 import React, { Component } from "react"
-import { View, Text, TouchableOpacity } from "react-native"
+import {
+  View,
+  Text,
+  TextInput,
+  ActivityIndicator,
+  Alert,
+  TouchableOpacity
+} from "react-native"
+import axios from "axios"
 
 class PostPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: "",
+      description: "",
+      isLoading: false,
+      statusMessage: ""
+    }
+  }
+
+  postNote = () => {
+    const url = "http://api.codefazz.com/api/Notes"
+    const { title, description, isLoading } = this.state
+    this.setState({ isLoading: true })
+
+    if (title === "") {
+      Alert.alert("Warning!", "Title can not be empty!")
+      return
+    }
+
+    const dataPayload = {
+      title: title,
+      content: description
+    }
+
+    axios
+      .post(url, dataPayload)
+      .then(response => {
+        console.log(response)
+        this.setState({ isLoading: false, statusMessage: 'POST Request success!' })
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState({ isLoading: false, statusMessage: 'POST Request failed!' })
+      })
+  }
+
   render() {
     return (
-        <View>
-          <Text>Post Page</Text>
-          <TouchableOpacity onPress={() => this.props.backToMainPage("main")}>
-            <Text style={{ margin: 10, backgroundColor: 'red' }}>Back to Main Page</Text>
-          </TouchableOpacity>
-        </View>
-      )
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        {this.state.isLoading && <ActivityIndicator size="large" />}
+        <Text> {this.state.statusMessage} </Text>
+        <TextInput
+          placeholder="Title"
+          onChangeText={txt => this.setState({ title: txt })}
+          style={{ width: "90%", backgroundColor: "yellow" }}
+        />
+        <TextInput
+          placeholder="Description"
+          onChangeText={txt => this.setState({ description: txt })}
+          style={{ width: "90%", backgroundColor: "yellow", marginTop: 5 }}
+        />
+        <Text>Post Page</Text>
+        <TouchableOpacity onPress={() => this.postNote()}>
+          <Text style={{ margin: 10, backgroundColor: "green" }}>
+            Post my Note!
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.props.backToMainPage("main")}>
+          <Text style={{ margin: 10, backgroundColor: "red" }}>
+            Back to Main Page
+          </Text>
+        </TouchableOpacity>
+      </View>
+    )
   }
 }
 
